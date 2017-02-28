450c99517d36aca49a1e5843d2c1058d7626a5e4
Kouhei0209 <kouhei19960209@gmail.com>
2017-02-28 10:05:48 +0900

    ？

--- day1/js/day1.js
+++ day1/js/day1.js
@@ -1,4 +1,4 @@
-var main = document.getElementByClassName("main");
+var main = document.getElementsByClassName("main");
 main[0].insertAdjacentHTML('afterbegi', '<p> Learning JavaScript </p>');
 
 
@@ -13,7 +13,7 @@ var Cat = function (name) {
     this.name = name;
     this.run = function () {
         console.log(this.name + "頑張って走る");
-    }:
+    };
 };
 
 Cat.prototype.run = function () {
